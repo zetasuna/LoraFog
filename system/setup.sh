70831eef7a9f22cdbaebaf121c7fc71d13f9f1bc
Nguyen Duc Nam <nguyenducnam2107@gmail.com>
2025-12-05 11:47:00 +0700

    Add file system

--- system/setup.sh
+++ system/setup.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 # --- Cài đặt udev rules ---
-if [ -f ./99-lora.rules ]; then
+if [! -f /etc/udev/rules.d/99-lora.rules ]; then
   sudo cp ./99-lora.rules /etc/udev/rules.d/
   sudo udevadm control --reload-rules
   sudo udevadm trigger
 else
-  echo "⚠️  Không tìm thấy file ./99-lora.rules — bỏ qua bước copy udev rules."
+  echo "⚠️  Tìm thấy file 99-lora.rules — bỏ qua bước copy udev rules."
+fi
+
+# --- Copy service vào ~/.local/bin nếu file tồn tại ---
+mkdir -p "$HOME/.local/bin"
+if [! -f "$HOME/.config/systemd/user/lora.service" ]; then
+  cp ./lora.service "$HOME/.config/systemd/user"
+else
+  echo "⚠️  Tìm thấy file lora.service — bỏ qua bước copy."
 fi
 
 # --- Tạo thư mục ~/.local/bin nếu chưa tồn tại ---
 mkdir -p "$HOME/.local/bin"
-
-# --- Copy service vào ~/.local/bin nếu file tồn tại ---
-if [ -f ./lora.service ]; then
-  cp ./lora.service "$HOME/.local/bin"
-else
-  echo "⚠️  Không tìm thấy file ./lora.service — bỏ qua bước copy."
-fi
 
 # --- Thêm ~/.local/bin vào PATH nếu chưa có ---
 case ":$PATH:" in
